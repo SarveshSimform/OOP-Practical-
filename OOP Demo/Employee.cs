637170621ef73f0ef410669095af7ae6634b6120
SF-LAP-729\sarvesh <sarvesh.p@simformsolutions.com>
2023-01-17 23:09:28 +0530

    Add project files.

---
+++ OOP Demo/Employee.cs
@@ -0,0 +1,41 @@
+﻿namespace OOP_Demo
+{
+    public class Employee
+    {
+        private float _salary;
+
+        public Employee(float salary)
+        {
+            this._salary = salary;
+        }
+
+        public void GetEmployeeDetails()
+        {
+            Console.WriteLine("Details of the Employee are following there : ");
+            Console.WriteLine("Salary : " + _salary);
+        }
+
+        public static class CompanyDetail
+        {
+            public static string Company = "Simform Solutions";
+            public static void GetCompanyName()
+            {
+                Console.WriteLine("Company Name :" + Company);
+            }
+        }
+        
+        public static void Main(string[] args)
+        {
+            Employee emp = new Employee(25000);
+            emp.GetEmployeeDetails();
+            Details details = new Details();
+            CompanyDetail.GetCompanyName();
+            details.EmpId();
+            details.EmpName();
+            details.Age();
+            details.Designation();
+            details.DeptName = "Asp.Net";
+            details.DeptDetail();
+        }
+    }
+}
---
+++ OOP Demo/IEmployee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Demo
+{
+    internal interface IEmployee
+    {
+        public void EmpName();
+        public void  Designation();
+        public void EmpId();
+        public void Age();
+    }
+    public abstract class DeptDetails
+    {
+        protected internal string? DeptName;
+        protected internal string? EmpDesignation;
+        protected internal string empDesignation
+        {
+            get
+            {
+                return EmpDesignation;
+            }
+            set
+            {
+                if (EmpDesignation != null)
+                {
+                    throw new Exception("Please enter Designation of the employee");
+                }
+                else
+                {
+                    EmpDesignation = value;
+                }
+            }
+        }
+        protected internal abstract void DeptDetail();
+    }
+}
